c9e1ae9d9a3d414e164e5c3937929899ecdd6e59
Kevin Traini <kevin@ktraini.com>
2023-03-27 18:56:04 +0200

    Add ParameterBag

---
+++ src/Bag/ParameterBag.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * MIT License
+ *
+ * Copyright (c) 2023-Present Kevin Traini
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace Marmotte\Http\Bag;
+
+/**
+ * Container for key/value pairs
+ */
+class ParameterBag implements \Countable
+{
+    /**
+     * @param array<string, mixed> $parameters
+     */
+    public function __construct(
+        protected array $parameters = []
+    ) {
+    }
+
+    public function keys(): array
+    {
+        return array_keys($this->parameters);
+    }
+
+    /**
+     * @param array<string, mixed> $parameters
+     */
+    public function add(array $parameters): void
+    {
+        $this->parameters = array_replace($this->parameters, $parameters);
+    }
+
+    public function get(string $key, mixed $default = null): mixed
+    {
+        return array_key_exists($key, $this->parameters) ? $this->parameters[$key] : $default;
+    }
+
+    public function set(string $key, mixed $value): void
+    {
+        $this->parameters[$key] = $value;
+    }
+
+    public function has(string $key): bool
+    {
+        return array_key_exists($key, $this->parameters);
+    }
+
+    public function remove(string $key): void
+    {
+        unset($this->parameters[$key]);
+    }
+
+    public function count(): int
+    {
+        return count($this->parameters);
+    }
+}
